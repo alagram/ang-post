6aa081c122a7ef9b52703d1a2155ea740f219533
Albert Agram <albert.agram@gmail.com>
2014-07-06 21:15:37 +0000

    updated posts controller

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,16 +1,10 @@
 'use strict';
 
-app.controller('PostCtrl', function($scope, $location, Post) {
+app.controller('PostsCtrl', function($scope, $location, Post) {
   $scope.posts = Post.all;
-  $scope.post = {url:'http://', title:''};
-
-  $scope.submitPost = function() {
-    Post.create($scope.post).then(function(ref) {
-      $location.path('/posts/' + ref.name());
-    });
-  };
 
   $scope.deletePost = function(postId) {
     Post.delete(postId);
   };
+
 });
